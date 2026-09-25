a323fff06a8a0ab2d80bc4b552c03b7303d14f6c
nkiliddar <Nkiliddar@gmail.com>
2026-09-25 13:54:38 -0400

    Update server.py

--- lab5_MCP_on_ACA/server/server.py
+++ lab5_MCP_on_ACA/server/server.py
@@ -4,37 +4,37 @@ import json
 
 # Initialize FastMCP server
 mcp = FastMCP(
-    name="kuljot-mcp-server",
+    name="circuits-mcp-server",
     host="0.0.0.0",
     port=8000
 )
 
 @mcp.tool(
-    name="get_recipes_by_name",
-    description="Get recipes based on the name provided by the user.",
+    name="get_circuits_by_Billing_number",
+    description="Get circuits id based on the Billing_number provided by the user.",
     structured_output=True
 )
-def get_recipes(name: str) -> Dict[str, Any]:
-    with open("recipes.json", "r") as file:
-        recipes = json.load(file)
-    for recipe in recipes:
-        if recipe["name"].lower() == name.lower():
-            return recipe
-    return {"error": "Recipe not found"}
+def get_circuit(name: int) -> Dict[str, Any]:
+    with open("circuits.json", "r") as file:
+        circuits = json.load(file)
+    for circuit in circuits:
+        if circuit["name"].lower() == name.lower():
+            return circuit
+    return {"error": "circuit not found"}
 
 
 @mcp.tool(
-    name="get_recipes_by_id",
-    description="Get recipes based on the ID provided by the user.",
+    name="get_circuits_by_id",
+    description="Get circuits based on the ID provided by the user.",
     structured_output=True
 )
-def get_recipes_by_id(id: int) -> Dict[str, Any]:
-    with open("recipes.json", "r") as file:
-        recipes = json.load(file)
-    for recipe in recipes:
-        if recipe["id"] == id:
-            return recipe
-    return {"error": "Recipe not found"}
+def get_circuit_by_id(id: int) -> Dict[str, Any]:
+    with open("circuits.json", "r") as file:
+        circuits = json.load(file)
+    for circuit in circuits:
+        if circuit["id"] == id:
+            return circuit
+    return {"error": "circuit not found"}
 
 @mcp.tool(
     name="get_courses",
